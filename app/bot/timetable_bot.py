bf14e9ddf98e47e364f3285dba1a9081e4acb1c0
german <germikgerman@gmail.com>
2021-09-29 15:22:00 +0300

    commands list added

--- app/bot/timetable_bot.py
+++ app/bot/timetable_bot.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import timedelta
+from typing import List
 
 import telebot
 import telebot.types as types
@@ -15,6 +16,7 @@ log = logging.getLogger(__name__)
 
 
 def start():
+    bot.set_my_commands(_get_commands())
     bot.polling(none_stop=True)
 
 
@@ -24,14 +26,7 @@ def cmd_start(message):
     bot.send_message(message.chat.id,
                      f"Привет! Я бот расписаний СПбГУ. Давай немного расскажу про себя. \n"
                      f"Я буду присылать тебе расписание каждый день в {config.SCHEDULER_TIME}.\n"
-                     f"Доступные команды: \n"
-                     f"/day - получить расписание на день.\n"
-                     f"/tom - получить расписание на завтра.\n"
-                     f"/retry - пройти регистрацию заново, если что-то пойдёт не так.\n"
-                     f"/unsubscribe - отписаться от рассылки.\n"
-                     f"/subscribe - подписаться повторно.\n"
-                     f"/commands - узнать доступные команды.\n"
-                     )
+                     f"Доступные команды: {_get_commands_as_str()}")
     log.debug('cmd_start(chat_id=%s)', message.chat.id)
     response = register_usecase.start(message.chat.id)
     bot.send_message(message.chat.id, response.text, reply_markup=_get_buttons(2, response.buttons))
@@ -70,21 +65,21 @@ def entering_year(message):
 def entering_group(message):
     log.debug('entering_group(chat_id=%s, group=%s)', message.chat.id, message.text)
     response = register_usecase.entering_group(message.chat.id, message.text)
-    bot.send_message(message.chat.id, response.text, reply_markup=_get_buttons(1, response.buttons))
+    bot.send_message(message.chat.id, response.text, reply_markup=types.ReplyKeyboardRemove())
 
 
 @bot.message_handler(commands=["unsubscribe"])
 def unsubscribe(message):
     log.debug('unsubscribe(chat_id=%s)', message.chat.id)
     response = register_usecase.unsubscribe(message.chat.id)
-    bot.send_message(message.chat.id, response.text)
+    bot.send_message(message.chat.id, response.text, reply_markup=types.ReplyKeyboardRemove())
 
 
 @bot.message_handler(commands=["subscribe"])
 def subscribe(message):
     log.debug('subscribe(chat_id=%s)', message.chat.id)
     response = register_usecase.subscribe(message.chat.id)
-    bot.send_message(message.chat.id, response.text)
+    bot.send_message(message.chat.id, response.text, reply_markup=types.ReplyKeyboardRemove())
 
 
 # Команды получения данных
@@ -92,7 +87,7 @@ def subscribe(message):
 def get_day_events(message):
     log.debug('get_day_events(chat_id=%s)', message.chat.id)
     response = event_usecase.get_day_events(time_util.get_current_date(), message.chat.id)
-    bot.send_message(message.chat.id, response.text)
+    bot.send_message(message.chat.id, response.text, reply_markup=types.ReplyKeyboardRemove())
 
 
 @bot.message_handler(commands=["tom"])
@@ -100,25 +95,20 @@ def get_tomorrow_events(message):
     log.debug('get_tomorrow_events(chat_id=%s)', message.chat.id)
     day = time_util.get_current_date() + timedelta(days=1)
     response = event_usecase.get_day_events(day, message.chat.id)
-    bot.send_message(message.chat.id, response.text)
+    bot.send_message(message.chat.id, response.text, reply_markup=types.ReplyKeyboardRemove())
 
 
 @bot.message_handler(commands=["commands"])
 def get_commands(message):
     log.debug('get_commands(chat_id=%s)', message.chat.id)
-    bot.send_message(message.chat.id, f"Доступные команды: \n"
-                                      f"/day - получить расписание на день.\n"
-                                      f"/tom - получить расписание на завтра.\n"
-                                      f"/retry - пройти регистрацию заново, если что-то пойдёт не так.\n"
-                                      f"/unsubscribe - отписаться от рассылки.\n"
-                                      f"/subscribe - подписаться повторно.\n"
-                                      f"/commands - узнать доступные команды.\n"
-                                      )
+    bot.send_message(message.chat.id, f"Доступные команды: {_get_commands_as_str()}",
+                     reply_markup=types.ReplyKeyboardRemove())
 
 
 def send_day_events():
     log.debug('send_day_events()')
-    event_usecase.get_day_events_all(bot.send_message)
+    event_usecase.get_day_events_all(
+        lambda chat_id, text: bot.send_message(chat_id, text, reply_markup=types.ReplyKeyboardRemove()))
 
 
 def _get_buttons(row_width, items):
@@ -126,3 +116,18 @@ def _get_buttons(row_width, items):
     buttons = list(map(lambda item: types.KeyboardButton(text=item), items))
     markup.add(*buttons)
     return markup
+
+
+def _get_commands_as_str() -> str:
+    return ''.join(map(lambda command: '\n/' + command.command + ' - ' + command.description, _get_commands()))
+
+
+def _get_commands() -> List[types.BotCommand]:
+    return [
+        types.BotCommand(command="day", description="получить расписание на день"),
+        types.BotCommand(command="tom", description="получить расписание на завтра"),
+        types.BotCommand(command="retry", description="пройти регистрацию заново, если что-то не так"),
+        types.BotCommand(command="unsubscribe", description="отписаться от рассылки"),
+        types.BotCommand(command="subscribe", description="подписаться повторно"),
+        types.BotCommand(command="commands", description="узнать доступные команды")
+    ]
